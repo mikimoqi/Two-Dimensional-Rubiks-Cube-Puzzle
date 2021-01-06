4ef8bd9650c42d4160f0f685aeb8f236dad4f5aa
Miki <53579064+supermikii@users.noreply.github.com>
2021-01-06 12:08:21 -0800

    Update HSLAPixel.cpp

--- Background util/HSLAPixel.cpp
+++ Background util/HSLAPixel.cpp
@@ -1,11 +1,3 @@
-/**
- * @file HSLAPixel.cpp
- * Implementation of the HSLAPixel class for use in with the PNG library.
- *
- * @author CS 225: Data Structures
- * @version 2018r1
- */
-
 #include "HSLAPixel.h"
 #include <cmath>
 #include <iostream>
